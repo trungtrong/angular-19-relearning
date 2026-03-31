34b1d38b31c16d73e490546765a785806d93e99e
Trong Ngo <trong.ngotrung@gmail.com>
2026-03-31 16:48:10 +0700

    [Models] Set up BaseModel

---
+++ src/app/shared/models/base.model.ts
@@ -0,0 +1,15 @@
+export class BaseModel {
+    id?: string;
+    createdAt?: Date | string;
+    createdBy?: string;
+    updatedAt?: Date | string;
+    updatedBy?: string;
+    deletedAt?: Date | string;
+    deletedBy?: string;
+    isDeleted?: boolean;
+    isError?: boolean;
+
+    constructor(init?: Partial<BaseModel>) {
+        Object.assign(this, init);
+    }
+}
